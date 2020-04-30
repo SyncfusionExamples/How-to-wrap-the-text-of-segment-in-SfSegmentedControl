5d018c1000fe00443aa3c01ed7eed51aa17b294e
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-04-30 18:07:42 +0530

    Sample included

---
+++ Wrap-Segment-text/SimpleSample/SimpleSample.Android/CustomLabelRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SimpleSample.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using static SimpleSample.ViewModel;
+
+[assembly: ExportRenderer(typeof(CustomLabel), typeof(CustomLabelRenderer))]
+namespace SimpleSample.Droid
+{
+    public class CustomLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.SetSingleLine(false);
+        }
+    }
+}
---
+++ Wrap-Segment-text/SimpleSample/SimpleSample/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Syncfusion.XForms.Buttons;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ViewModel 
+    {
+        public ObservableCollection<View> ViewItems { get; set; }
+
+        public ViewModel()
+        {
+            ViewItems = new ObservableCollection<View>()
+            {
+                new CustomLabel(){Text = "Veg (Garlic, taco toppings and ranch)" },
+                new CustomLabel(){Text = "Non Veg (bafbecue sauce and chicken)" }
+            };
+         }
+    }
+
+    public class CustomLabel : Label
+    {
+        public CustomLabel()
+        {
+            TextColor = Color.Black;
+            FontSize = 30;
+            LineBreakMode = Xamarin.Forms.LineBreakMode.WordWrap;
+            HorizontalTextAlignment = TextAlignment.Center;
+            VerticalOptions = LayoutOptions.Center;
+            HeightRequest = 200;
+        }
+    }
+
+}
